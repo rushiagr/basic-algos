de0f6769319a01381e3e24caeb8ae8f733c1811f
Rushi Agrawal <rushi.agr@gmail.com>
2013-05-24 13:25:52 +0530

    coin change problem added

---
+++ dynamic-programming/coin-change-infinite-supply.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+
+/*
+ * Given a set of coins (each coin can be used multiple times), and a change amount, return the minimum number of coins required to make the change. Return -1 if not possible to make change. 
+ * To make the problem non-dp, just remove the lines which are concerned with the array 'dp'
+ */
+int mincoins(int *arr, int n, int sum) {
+
+    static int dp[10000] = {0};
+    
+    if(dp[sum]!=0)
+        return dp[sum];    
+    if(sum==0)
+        return 0;
+    if(sum<0)
+        return -1;
+    
+    int when_1_consumed = sum+1;
+
+    for(int i=0; i<n; i++) {
+        int result = mincoins(arr, n, sum-arr[i]);
+        if(result==0) {
+            dp[sum] = 1;
+            return 1;
+        }
+        if(result!=-1 && result<when_1_consumed) {
+            when_1_consumed = result;
+        }
+    }
+    
+    if (when_1_consumed==sum+1) {
+        dp[sum] = -1;
+        return -1;
+    }
+    dp[sum] = when_1_consumed+1;
+    return when_1_consumed + 1;
+}
+
+int main() {
+    int a[] = {2,4};
+//    int b[] = {2,5,5};
+    cout<<mincoins(a, 2, 3000);
+//    cout<<mincoins(b, 3, 11);
+    
+    return 0;
+}
---
+++ tp.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int dp[1000000] = {0};
+
+int main() {
+    for(int i=0; i<10; i++)
+        cout<<dp[i];
+    return 0;
+}
